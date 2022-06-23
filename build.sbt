bfe22c6e8765dc6e383936076bcb57d1febf2e3f
danven72 <danven@MBP-di-Danilo.lan>
2022-06-23 20:24:24 +0200

    Mockito added

--- build.sbt
+++ build.sbt
@@ -3,6 +3,7 @@ ThisBuild / version := "0.1.0-SNAPSHOT"
 ThisBuild / scalaVersion := "2.13.8"
 
 val scalaTestVersion = "3.2.12"
+val mockitoVersion   = "3.2.12.0"
 
 lazy val root = (project in file("."))
   .settings(
@@ -10,6 +11,7 @@ lazy val root = (project in file("."))
   )
 
 libraryDependencies ++= Seq(
-  "org.scalatest" %% "scalatest" % scalaTestVersion,
-  "org.scalatest" %% "scalatest" % scalaTestVersion % "test"
+  "org.scalatest"     %% "scalatest"   % scalaTestVersion,
+  "org.scalatest"     %% "scalatest"   % scalaTestVersion % "test",
+  "org.scalatestplus" %% "mockito-4-5" % mockitoVersion   % "test"
 )
--- src/test/scala/it/bitrock/goosgame/GooseGameSpec.scala
+++ src/test/scala/it/bitrock/goosgame/GooseGameSpec.scala
@@ -1,14 +1,77 @@
 package it.bitrock.goosgame
 
-import it.bitrock.goosgame.outcomes.OutcomeResultBuilder
+import it.bitrock.goosgame.outcomes.{Outcome, OutcomeResultBuilder}
+import org.mockito.Mockito._
 import org.scalatest.funsuite.AnyFunSuite
+import org.scalatestplus.mockito.MockitoSugar
 
-// TODO try to Test using Mock OutcomeResultBuilder
-class GooseGameSpec extends AnyFunSuite {
-  val outcomeResultBuilder = new OutcomeResultBuilder
-  val goose                = GooseGame(outcomeResultBuilder)
+class GooseGameSpec extends AnyFunSuite with MockitoSugar {
+  val mockOutcomeResultBuilder = mock[OutcomeResultBuilder]
+  val goose                    = GooseGame(mockOutcomeResultBuilder)
 
   test("testAddNewPlayer") {
-    val resultJohn = goose.addPNewPlayer("John")
+    val newPlayer = "Tom"
+    goose.addPNewPlayer(newPlayer)
+    verify(mockOutcomeResultBuilder, times(1)).buildAddPlayerOutcomeResult(false, List(newPlayer))
+
+    goose.addPNewPlayer(newPlayer)
+    verify(mockOutcomeResultBuilder, times(1)).buildAddPlayerOutcomeResult(true, List(newPlayer))
+  }
+
+  test("testExit") {
+    goose.exit()
+    verify(mockOutcomeResultBuilder, times(1)).buildExitOutcomeResult()
+  }
+
+  test("testUnknownInput") {
+    val input = "Unknown"
+    goose.unknownInput(input)
+    verify(mockOutcomeResultBuilder, times(1)).buildUnknownCommandOutcomeResult(input)
+  }
+
+  test("testMovePlayer externalDices and dice not valid") {
+    val dices = (23, 2)
+    goose.movePlayer("tom", dices)
+    verify(mockOutcomeResultBuilder, times(1)).buildDicesNotValidOutcomeResult(dices)
+  }
+
+  test("testMovePlayer when player not exists") {
+    val player = "Bill"
+    goose.movePlayer(player)
+    verify(mockOutcomeResultBuilder, times(1)).buildPlayerNotFoundOutcomeResult(player)
+  }
+
+  test("testMovePlayer ordinary Outcome") {
+    val player = "Ben"
+    goose.addPNewPlayer(player)
+    val dices    = (2, 2)
+    val ordinary = Outcome(player, dices, 0, 4, 4, None)
+    goose.movePlayer(player, dices)
+    verify(mockOutcomeResultBuilder, times(1)).buildMoveOutcomeResult(List(ordinary))
+  }
+
+  test("testMovePlayer single Goose") {
+    val player = "Thor"
+    goose.addPNewPlayer(player)
+    val dices        = (2, 3)
+    val gooseOutcome = Outcome(player, dices, 0, 5, 8, None)
+    goose.movePlayer(player, dices)
+    verify(mockOutcomeResultBuilder, times(1)).buildMoveOutcomeResult(List(gooseOutcome))
+  }
+
+  test("testMovePlayer double Goose") {
+    goose.resetPlayers() //Why this happen?
+    val player = "Thor"
+    goose.addPNewPlayer(player)
+    val dices1    = (5, 5)
+    val ordinary1 = Outcome(player, dices1, 0, 10, 10, None)
+    goose.movePlayer(player, dices1)
+    verify(mockOutcomeResultBuilder, times(1)).buildMoveOutcomeResult(List(ordinary1))
+
+    val dices2       = (2, 2)
+    val gooseOutcome = Outcome(player, dices2, 10, 14, 18, None)
+    val ordinary2    = Outcome(player, dices2, 18, 22, 22, None)
+    goose.movePlayer(player, dices2)
+    verify(mockOutcomeResultBuilder, times(1)).buildMoveOutcomeResult(List(gooseOutcome, ordinary2))
   }
 }
